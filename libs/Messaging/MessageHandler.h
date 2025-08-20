1448d6048f191e78e71d93f7774cb59ce3b75343
Reyhanehkabiri <ry.kabiri@gmail.com>
2025-08-21 01:37:21 +0330

    MessageHandler.h
    اضافه کردن قابلیت های باقی مونده

--- libs/Messaging/MessageHandler.h
+++ libs/Messaging/MessageHandler.h
@@ -6,7 +6,9 @@
 #include <sstream>
 #include <io.h>
 #include <fcntl.h>
-#include <algorithm> // برای توابع جستجو
+#include <algorithm>
+#include <codecvt>
+#include <locale>
 
 struct Message {
     std::wstring id;
@@ -16,8 +18,14 @@ struct Message {
     time_t timestamp;
     bool is_read = false;
     bool is_editable = true;
+    bool is_deleted = false;
+    std::wstring replied_to_id;
+    bool is_forwarded = false;
+    // اضافه کردن فیلدهای جدید برای فایل
+    std::wstring file_path;   // مسیر فایل
+    std::wstring file_name;   // نام نمایشی فایل
+    bool has_attachment = false; // آیا پیام ضمیمه دارد؟
 
-    // نمایش زمان به فرمت میلادی
     std::wstring get_formatted_time() const {
         std::tm tm;
 #ifdef _WIN32
@@ -35,7 +43,6 @@ class MessageManager {
 private:
     static std::vector<Message> messages;
 
-    // تابع کمکی برای جستجوی غیرحساس به حروف
     static bool contains_ignore_case(const std::wstring& str, const std::wstring& keyword) {
         auto it = std::search(
                 str.begin(), str.end(),
@@ -47,19 +54,27 @@ private:
         return it != str.end();
     }
 
+    static const Message* find_message(const std::wstring& id);
+
 public:
-    static void send(const Message& msg);
+    static void send(const Message& msg, const std::wstring& attachment_path = L"");
     static bool edit_message(const std::wstring& id, const std::wstring& new_content);
+    static bool delete_message(const std::wstring& id);
+    static bool forward_message(const std::wstring& id, const std::wstring& new_receiver);
+    static Message create_reply(const std::wstring& original_id, const std::wstring& sender,
+                                const std::wstring& reply_content);
     static std::vector<Message> get_last_messages(int limit = 10);
     static int get_unread_count(const std::wstring& user);
+    static bool containsEmoji(const std::wstring& text);
+    static std::wstring filterEmojis(const std::wstring& text);
 
-    // تابع جستجوی پیشرفته
     static std::pair<int, std::vector<Message>> search_messages(const std::wstring& keyword) {
         std::vector<Message> results;
         for (const auto& msg : messages) {
-            if (contains_ignore_case(msg.content, keyword) ||
-                contains_ignore_case(msg.sender, keyword) ||
-                contains_ignore_case(msg.receiver, keyword)) {
+            if (!msg.is_deleted &&
+                (contains_ignore_case(msg.content, keyword) ||
+                 contains_ignore_case(msg.sender, keyword) ||
+                 contains_ignore_case(msg.receiver, keyword))) {
                 results.push_back(msg);
             }
         }

b2cb48a73c9a4a8f79b3fb6aff2fbae938ff0e37
Arjp80 <ajjj47726@gmail.com>
2025-08-20 19:56:04 +0330

    Update UserManager.cpp

--- libs/User/UserManager.cpp
+++ libs/User/UserManager.cpp
@@ -1,3 +1,66 @@
-//
-// Created by M on 7/28/2025.
-//
+#include "UserManager.h"
+#include <iostream>
+#include <functional>
+
+
+UserManager::UserManager(Database& db) : database(db), currentUser("") {}
+
+bool UserManager::registerUser(const std::string& username, const std::string& password) {
+    std::string hashed = hashPassword(password);
+    return database.createAccount(username, hashed);
+}
+
+
+bool UserManager::loginUser(const std::string& username, const std::string& password) {
+    std::string hashed = hashPassword(password);
+    if (database.login(username, hashed)) {
+        currentUser = username;
+        return true;
+    }
+    return false;
+}
+
+void UserManager::logoutUser() {
+    currentUser.clear();
+}
+
+bool UserManager::sendMessageToUser(const std::string& receiver, const std::string& content) {
+    if (!isLoggedIn()) return false;
+    return database.sendMessage(currentUser, receiver, content);
+}
+
+bool UserManager::sendMessageToChatroom(const std::string& chatroomName, const std::string& content) {
+    if (!isLoggedIn()) return false;
+    return database.sendMessage(currentUser, chatroomName, content);
+}
+
+
+bool UserManager::isLoggedIn() const {
+    return !currentUser.empty();
+}
+
+std::string UserManager::getCurrentUser() const {
+    return currentUser;
+}
+
+std::string UserManager::hashPassword(const std::string& password) {
+    std::hash<std::string> hasher;
+    return std::to_string(hasher(password));
+}
+
+bool UserManager::deleteUser(const std::string& username) {
+    if (!isLoggedIn() || currentUser != username) {
+        std::cout << "خطا: فقط می‌توانی حساب خودتو حذف کنی.\n";
+        return false;
+    }
+
+    if (database.deleteAccount(username)) {
+        std::cout << "حساب کاربری با موفقیت حذف شد.\n";
+        logoutUser();
+        return true;
+    }
+    else {
+        std::cout << "خطا در حذف حساب کاربری.\n";
+        return false;
+    }
+}

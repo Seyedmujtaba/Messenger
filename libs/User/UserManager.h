678bc3008a27faf28adea3d7fa15c15788b606f8
Arjp80 <ajjj47726@gmail.com>
2025-08-20 16:44:46 +0330

    Update UserManager.h

--- libs/User/UserManager.h
+++ libs/User/UserManager.h
@@ -1,8 +1,25 @@
-//
-// Created by M on 7/28/2025.
-//
-
 #ifndef USERMANAGER_H
 #define USERMANAGER_H
 
-#endif //USERMANAGER_H
+#include <string>
+#include "Database.h"
+
+class UserManager {
+public:
+     UserManager(Database& db);
+
+    bool registerUser(const std::string& username, const std::string& password);
+    bool loginUser(const std::string& username, const std::string& password);
+    void logoutUser();
+    bool deleteUser(const std::string& username); 
+    bool isLoggedIn() const;
+    std::string getCurrentUser() const;
+
+private:
+    Database& database;
+    std::string currentUser;
+
+    std::string hashPassword(const std::string& password);
+};
+
+#endif 
